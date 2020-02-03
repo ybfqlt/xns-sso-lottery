786d6fa9b29011784e01befc1c2a2d892a4c14c6
ltt <823030735@qq.com>
2020-02-03 19:14:04 +0800

    对逻辑进行修改

--- xns-sso-server/src/main/java/com/ns/sso/service/impl/LonginServiceImpl.java
+++ xns-sso-server/src/main/java/com/ns/sso/service/impl/LonginServiceImpl.java
@@ -7,6 +7,7 @@ import com.ns.sso.service.LoginService;
 import com.ns.sso.service.consumer.RedisClientService;
 import com.ns.sso.utils.IdWorker;
 import com.ns.sso.utils.JsonUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
  * @Author: xns
  * @Date: 20-2-1 下午8:35
  */
+@Slf4j
 @Service
 public class LonginServiceImpl implements LoginService {
 
@@ -28,38 +30,45 @@ public class LonginServiceImpl implements LoginService {
     public void register(User user) {
         IdWorker idWorker = new IdWorker();
         user.setUserId(String.valueOf(idWorker.nextId()));
-        if(userMapper.insert(user));
+        if (userMapper.insert(user)) ;
     }
 
     /**
      * 登录逻辑
+     *
      * @param user
      * @return
      */
     @Override
-    public User login(User user) throws Exception {
+    public User login(User user) {
         String json = redisClientService.get(user.getUserName());
 
-        if(StatusConstants.STATUS_NO.equals(json)){
+        if (StatusConstants.STATUS_NO.equals(json)) {
             User user1 = userMapper.findByName(user.getUserName());
-            if(user1 == null){
+            if (user1 == null) {
                 return null;
             }
-            if(user1.getUserPassword().equals(user.getUserPassword())){
-                String json1 = JsonUtils.objToJson(user);
-                redisClientService.put(user.getUserName(),json1,60*5);
+            if (user1.getUserPassword().equals(user.getUserPassword())) {
+                String json1 = null;
+                try {
+                    json1 = JsonUtils.objToJson(user);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                redisClientService.put(user.getUserName(), json1, 60 * 60);
                 return user1;
-            }
-            else{
+            } else {
                 return null;
             }
-        }else if(StatusConstants.STATUS_YES.equals(json)){
-            User user1 = JsonUtils.jsonToPojo(json,User.class);
-            if(user.getUserPassword().equals(user1.getUserPassword())){
-                return user;
-            }
-            else{
-                return null;
+        } else {
+            User user1 = null;
+            try {
+                user1 = JsonUtils.jsonToPojo(json, User.class);
+                if (user.getUserPassword().equals(user1.getUserPassword())) {
+                    return user;
+                }
+            } catch (Exception e) {
+                log.warn("熔断:{}", e.getMessage());
             }
         }
         return null;
